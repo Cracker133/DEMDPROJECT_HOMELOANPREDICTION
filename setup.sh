0b573623d4f93a25bb8616495355d8db206a3eca
Vamsi <86515605+Cracker133@users.noreply.github.com>
2021-10-27 23:31:29 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
 mkdir -p ~/.streamlit/
 echo "\
+[general]\n\
+email = \"your-email@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
 [server]\n\
 headless = true\n\
+enableCORS=false\n\
 port = $PORT\n\
-enableCORS = false\n\
-\n\
 " > ~/.streamlit/config.toml
+© 2021 GitHub, Inc.
+Terms
+Pri
